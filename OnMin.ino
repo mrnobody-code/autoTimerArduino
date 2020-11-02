126bcbccff443bf29e21d8dafe4288c87c0aa6ca
mrnobody-code <69108586+mrnobody-code@users.noreply.github.com>
2020-11-02 21:59:59 +0530

    Update OnMin.ino

--- OnMin.ino
+++ OnMin.ino
@@ -1,7 +1,7 @@
 int i;
 
 int mint() {
-  
+  lcd.clear();
   // put your main code here, to run repeatedly:
   int a = digitalRead(button_right);
   if (a == 1) {
@@ -18,9 +18,21 @@ int mint() {
   if (b == 1) {
     i = i - 1;
     Serial.println(i);
+    lcd.setCursor(0,0);
+    lcd.print("Set Minutes");
     lcd.setCursor(0,1);
     lcd.print(i);
+    if(i == -1){
+      lcd.setCursor(0,1);
+      lcd.print("Invalid Minutes");
+    }
     delay(300);
+  }
+  if(i == 60){
+    i = 0;
+  }
+  if(i <= -1){
+     i = 0;
   }
   return i;
 }
