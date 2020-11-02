e976b51d01e34d77e005e276ad6caa4d41f4b3ce
mrnobody-code <69108586+mrnobody-code@users.noreply.github.com>
2020-11-02 21:59:27 +0530

    Update OnHr.ino

--- OnHr.ino
+++ OnHr.ino
@@ -1,7 +1,7 @@
 int j;
 
 int settings() {
-  
+  lcd.clear();
   // put your main code here, to run repeatedly:
   int a = digitalRead(button_up);
   if (a == 1) {
@@ -18,9 +18,21 @@ int settings() {
   if (b == 1) {
     j = j - 1;
     Serial.println(j);
+    lcd.setCursor(0,0);
+    lcd.print("Set Hour");
     lcd.setCursor(0,1);
     lcd.print(j);
+     if(j == -1){
+      lcd.setCursor(0,1);
+      lcd.print("Invalid Hours");
+    }
     delay(300);
+  }
+  if(j==12){
+   j = 0;
+  }
+   if(j <= -1){
+     j = 0;
   }
   return j;
 }
